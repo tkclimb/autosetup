55df00112601a5996b13a2636ebd74cceff7c84d
tkclimb <tkclimb0911@gmail.com>
2019-06-07 10:14:20 +0900

    Update

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ AS_PLATFORM="$OSTYPE"
 
 # initialize
 if [[ "$AS_PLATFORM" == "linux-gnu" ]]; then
-  # sudo apt update -y
-  # sudo apt upgrade -y
+  sudo apt update -y
+  sudo apt upgrade -y
 
   AS_BREW_DIR=/home/linuxbrew/.linuxbrew
   if [ ! -e "$AS_BREW_DIR" ]; then
@@ -27,9 +27,10 @@ fi
 
 # install basic packages
 if [[ "$AS_PLATFORM" == "linux-gnu" ]]; then
+  # basics
+  sudo apt install xsel
   # for pyenv
   sudo apt install -y --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
-
   # for rbenv
   sudo apt install -y autoconf bison libssl-dev libyaml-dev libreadline6-dev  libffi-dev libgdbm5 libgdbm-dev
 
